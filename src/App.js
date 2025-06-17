12ee58276aa1f672b9254c45c81b53bd8d553be4
Zelalem Getahun <zelalemg935@gmail.com>
2025-06-17 13:14:35 -0700

    react-router-dom imported and used

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
+import Detail from "./components/Detail";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
